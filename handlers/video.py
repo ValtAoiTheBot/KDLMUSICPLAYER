31c60a2f6056b904c73578efd3764de0ff1c5d41
ValtAoiTheBot <91861970+ValtAoiTheBot@users.noreply.github.com>
2021-10-31 18:52:44 +0530

    Update video.py

--- handlers/video.py
+++ handlers/video.py
@@ -5,7 +5,7 @@
 import asyncio
 import re
 
-from config import BOT_USERNAME, GROUP_SUPPORT, IMG_1, IMG_2, UPDATES_CHANNEL
+from config import BOT_USERNAME, GROUP_SUPPORT, UPDATES_CHANNEL
 from driver.filters import command, other_filters
 from driver.queues import QUEUE, add_to_queue
 from driver.veez import call_py
